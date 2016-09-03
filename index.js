5101a61e4f0f253616f83e356107c075191367fd
prajankya <prajankya@gmail.com>
2016-09-03 13:09:08 +0530

    added socket.io

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 var usage = require('./modules/usage');
 
 usage.init();
-var use = usage.poll();
+var curUsage = [];
+
+// serve index.html if root
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/public/index.html');
+});
+
+// serve static files from public directory
+app.use(express.static(__dirname + '/public'));
+
+// start
+var port = Number(process.env.PORT || 4000);
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
+
+io.on('connection', function(socket) {
+    socket.on('event', function(data) {
+        console.log("Event");
+        console.log(data);
+    });
+    socket.on('disconnect', function() {});
+});
+
 setInterval(function() {
     var use = usage.poll();
-    console.log(use);
-}, 3000);
+    if (use !== curUsage) {
+        curUsage = use;
+        io.emit("update_usage", curUsage);
+    }
+}, 2000);
+
+server.listen(3000);
